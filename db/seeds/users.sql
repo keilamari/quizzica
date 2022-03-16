c7657b1847afbb6e9c1c2cd60c3224b1e7fb12a0
saurabhdabas <developerdabas@gmail.com>
2022-03-16 01:51:08 +0000

    Add more users to the table

--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@email.com', '123');
-INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@email.com', '1233');
+INSERT INTO users (name, email, password) VALUES ('Saurabh', 'saurabh@email.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Keila', 'keila@email.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Ahmed', 'Ahmed@email.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@email.com', '123');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@email.com', '123');
