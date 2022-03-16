d9e8db4a4870b6b3a9b7fb0d7eaa5417376f2740
saurabhdabas <developerdabas@gmail.com>
2022-03-16 02:26:28 +0000

    Add new data

--- db/seeds/answers.sql
+++ db/seeds/answers.sql
@@ -1,8 +1,64 @@
-INSERT INTO answers (question_id, is_correct, value) VALUES (1, false, 'Not Answer');
-INSERT INTO answers (question_id, is_correct, value) VALUES (1, false, 'Not Answer');
-INSERT INTO answers (question_id, is_correct, value) VALUES (1, true, 'Answer');
-INSERT INTO answers (question_id, is_correct, value) VALUES (1, false, 'Not Answer');
-INSERT INTO answers (question_id, is_correct, value) VALUES (2, false, 'Not Answer');
-INSERT INTO answers (question_id, is_correct, value) VALUES (2, true, 'Answer');
-INSERT INTO answers (question_id, is_correct, value) VALUES (2, false, 'Not Answer');
-INSERT INTO answers (question_id, is_correct, value) VALUES (2, false, 'Not Answer');
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '4', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '3', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '6', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '0', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '3', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '2', TRUE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '0', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '3', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '2', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, '1', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, '5', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, '4', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, '4', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, '3', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, '1', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (6, '4', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (6, '5', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (6, '1', TRUE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (7, '7', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (7, '2', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (7, '6', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (8, '0', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (8, '9', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (8, '10', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (9, '0', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (9, '4', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (9, '8', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (10, '1', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (10, '35', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (10, '5', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (11, '8', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (11, '4', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (11, '0', TRUE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (12, '1', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (12, '2', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (12, '7', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (13, '1', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (13, '8', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (13, '34', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (14, '0', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (14, '2', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (14, '4', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (15, '6', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (15, '4', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (15, '4', TRUE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (16, '1', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (16, '4', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (16, '5', TRUE);
+
