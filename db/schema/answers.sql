8660002b202669ffdae236feeda76008de8bc0d0
saurabhdabas <developerdabas@gmail.com>
2022-03-16 02:26:03 +0000

    Modified attribute

--- db/schema/answers.sql
+++ db/schema/answers.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS answers CASCADE;
 CREATE TABLE answers (
   id SERIAL PRIMARY KEY NOT NULL,
-  question_id INTEGER REFERENCES questions(id),
-  is_correct BOOLEAN DEFAULT FALSE,
-  value TEXT
-  );
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  answer TEXT NOT NULL,
+  isCorrect BOOLEAN NOT NULL DEFAULT FALSE
+);
