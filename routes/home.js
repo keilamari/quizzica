3be719d99b79c142ed8737f38e2ed02682ee5fe3
saurabhdabas <developerdabas@gmail.com>
2022-03-16 03:04:35 +0000

    Add homepage which shows all public quizzes

---
+++ routes/home.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+  //home page to see public quizzes
+
+  router.get('/:user_id', (req, res) => {
+    db.query(`
+    SELECT * FROM quizzes WHERE isPrivate = FALSE;
+    `)
+    .then(data => {
+      const templateVar = {quizzes: data.rows, user_id: req.params.user_id};
+      res.render('../views/home', templateVar);
+    })
+  });
+  return router;
+};
