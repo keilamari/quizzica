1548242c49b9c0d03d9194f019575cc36e713196
saurabhdabas <developerdabas@gmail.com>
2022-03-16 17:21:19 +0000

    Removed a route

--- routes/home.js
+++ routes/home.js
@@ -12,14 +12,14 @@ module.exports = (db) => {
       res.render('../views/home', templateVar);
     })
   });
-  router.get('/:user_id', (req, res) => {
-    db.query(`
-    SELECT * FROM quizzes WHERE isPrivate = FALSE;
-    `)
-    .then(data => {
-      const templateVar = {quizzes: data.rows, user_id: req.params.user_id};
-      res.render('../views/home', templateVar);
-    })
-  });
+  // router.get('/:user_id', (req, res) => {
+  //   db.query(`
+  //   SELECT * FROM quizzes WHERE isPrivate = FALSE;
+  //   `)
+  //   .then(data => {
+  //     const templateVar = {quizzes: data.rows, user_id: req.params.user_id};
+  //     res.render('../views/home', templateVar);
+  //   })
+  // });
   return router;
 };
