e9bf3db332082024d6132ab6af7b86509ef9d0a9
saurabhdabas <developerdabas@gmail.com>
2022-03-16 02:10:53 +0000

    Add new data

--- db/seeds/questions.sql
+++ db/seeds/questions.sql
@@ -1,2 +1,19 @@
-INSERT INTO questions (quiz, question) VALUES (1, 'What is true');
-INSERT INTO questions (quiz, question) VALUES (1, 'What is the answer');
+INSERT INTO questions (quiz_id, question) VALUES (1, 'What is 2 + 2');
+INSERT INTO questions (quiz_id, question) VALUES (1, 'What is 1 + 1');
+INSERT INTO questions (quiz_id, question) VALUES (1, 'What is 1 + 2');
+INSERT INTO questions (quiz_id, question) VALUES (1, 'What is 0 + 1');
+
+INSERT INTO questions (quiz_id, question) VALUES (2, 'What is 2 * 2');
+INSERT INTO questions (quiz_id, question) VALUES (2, 'What is 1 * 1');
+INSERT INTO questions (quiz_id, question) VALUES (2, 'What is 1 * 2');
+INSERT INTO questions (quiz_id, question) VALUES (2, 'What is 0 *1');
+
+INSERT INTO questions (quiz_id, question) VALUES (3, 'What is 1 - 0');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'What is 2 - 1');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'What is 2 - 2');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'What is 3 - 2');
+
+INSERT INTO questions (quiz_id, question) VALUES (4, 'What is 2 / 2');
+INSERT INTO questions (quiz_id, question) VALUES (4, 'What is 4 / 2');
+INSERT INTO questions (quiz_id, question) VALUES (4, 'What is 8 / 2');
+INSERT INTO questions (quiz_id, question) VALUES (4, 'What is 10 / 2');
