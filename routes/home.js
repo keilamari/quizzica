a4366794fc8e06ac91a86e41afb0067eb8bb66f3
saurabhdabas <developerdabas@gmail.com>
2022-03-16 17:11:30 +0000

    changed home route

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 module.exports = (db) => {
   //home page to see public quizzes
-
+  router.get('/', (req, res) => {
+    db.query(`
+    SELECT * FROM quizzes WHERE isPrivate = FALSE;
+    `)
+    .then(data => {
+      const templateVar = {quizzes: data.rows, user_id: req.params.user_id};
+      res.render('../views/home', templateVar);
+    })
+  });
   router.get('/:user_id', (req, res) => {
     db.query(`
     SELECT * FROM quizzes WHERE isPrivate = FALSE;
