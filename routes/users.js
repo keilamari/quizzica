0bbffb11851fa45d2bd604022b497526c159c956
saurabhdabas <developerdabas@gmail.com>
2022-03-16 17:48:03 +0000

    Add a delete rquest

--- routes/users.js
+++ routes/users.js
@@ -25,5 +25,27 @@ module.exports = (db) => {
     let templateVar = { user_id: req.params.user_id };
     res.render('../views/makeQuiz', templateVar);
   })
+
+  router.post("/:user_id/makequiz", (req, res) => {
+    let query = `INSERT INTO quizzes (user_id, name, description, isPrivate)
+                VALUES ($1, $2, $3, $4) RETURNING id`;
+    let values = [req.params.user_id, req.body.name, req.body.description, req.body.isPrivate];
+      db.query(query, values)
+        .then(data => {
+          const quiz = data.rows;
+          let quizid = data.rows[0].id
+          res.redirect(`/quiz/${quizid}/questions`);
+        })
+      .catch(err => {
+        res.send(`Please click back and complete the form.`);
+      });
+    });
+    router.post("/:user_id/delete/:quiz_id", (req, res) => {
+      db.query(`DELETE FROM quizzes WHERE id = $1 AND user_id = $2;`,
+      [req.params.quiz_id, req.params.user_id])
+      .then(() => {
+        res.redirect(`/users/${req.params.user_id}`);
+      });
+    });
   return router;
 };
