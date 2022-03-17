57b450049af3593c4b4d9327b498f2399f912325
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-17 15:18:19 -0400

    updated schema and seeds

--- db/schema/attempts.sql
+++
@@ -1,9 +0,0 @@
--- Drop and recreate Attempts table (Example)
-
-DROP TABLE IF EXISTS attempts CASCADE;
-CREATE TABLE attempts (
-  id SERIAL PRIMARY KEY NOT NULL,
-  quiz_id INTEGER REFERENCES quizzes(id),
-  user_id INTEGER REFERENCES users(id),
-  score INTEGER
-  );
